12fa7aa9c702e681c4eec6108609393005d84ded
yonaverse <yon4sdev@outlook.com>
2024-12-17 20:00:10 +0300

    it's better to use Images in next.js instead of the normal html images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import Link from "next/link";
-export default function Home() {
+import Image from "next/image";
+import homepic from "/public/home.jpg";
+import "./globals.css"
+const Home = () => {
   return (
+    <>
+      <h1>hello from home</h1>
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homepic}
+          alt="home"
+          fill
+          style={{ objectFit: "cover" }} />
+      </div>
 
-   
-    <div>
-       <h1>hello wolrd</h1>
-
-    </div>
-  
+    </>
   );
-}
+};
+
+export default Home;
